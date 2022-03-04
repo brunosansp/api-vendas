815c47fd1da1dc2721cd93d0add99402dc9bc39e
Bruno Santos <brunoadsjp@gmail.com>
2022-03-03 21:53:25 -0300

    Primeiro commit com configs iniciais

---
+++ run.sh
@@ -0,0 +1,19 @@
+# Se não tiver o yarn instalado e caso queira usar ele.
+$ npm install --global yarn
+
+# Iniciando o projeto
+$ yarn init -y
+
+# Instalando depenências iniciais
+$ yarn add typescript ts-node-dev @types/node tsconfig-paths -D
+
+# Criando o tsconfig.json que conterá as configurações do Typescript
+npx tsc --init --rootDir src --outDir build --esModuleInterop --resolveJsonModule --lib es6 --module commonjs --allowJs true --noImplicitAny true
+
+# Compilando o código
+npx tsc
+
+# Adicionando script no package.json a configuração para rodar o projeto
+"scripts": {
+  "dev": "ts-node-dev --inspect --transpile-only --ignore-watch node_modules src/server.ts"
+}
